70149f70260bcecd6e85f875810f89692a3cb547
ssptechworld <ssptechworld@gmail.com>
2026-09-10 11:58:09 +0530

    11-58

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
 
 
